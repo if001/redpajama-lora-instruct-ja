0f4cfe886995cf8c6cc8b58e3a0503ea256f70b3
windows_on_wsl <otomijuf.004@gmail.com>
2023-05-11 02:09:29 +0900

    fix quantization

--- finetune.py
+++ finetune.py
@@ -242,7 +242,7 @@ def train(
     #
     # Model loading
     #
-    quantization_config = BitsAndBytesConfig(llm_int8_enable_fp32_cpu_offload=True)
+    # quantization_config = BitsAndBytesConfig(llm_int8_enable_fp32_cpu_offload=True)
     model = AutoModelForCausalLM.from_pretrained(
         # 'mosaicml/mpt-7b',
         base_model,
@@ -251,7 +251,7 @@ def train(
         load_in_8bit=True,
         torch_dtype=torch.float16,
         device_map=device_map,
-        quantization_config=quantization_config,
+        # quantization_config=quantization_config,
         # load_in_8bit_fp32_cpu_offload=True
     )
 
